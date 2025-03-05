807443501da79fd96c0501257d1b7e76791df3fa
ReekyStive <reekystive@gmail.com>
2025-03-05 21:14:07 +0800

    chore: 🔍 disable cspell for placeholder text in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ const Home: FC = () => {
       <h1 className="text-2xl font-bold">Alka</h1>
       <p className="text-sm">Alka web app.</p>
       <p>
+        {/* cspell:disable */}
         Id nisi adipisicing aute laborum consequat nostrud quis officia consectetur esse et aliqua. Anim ea
         reprehenderit pariatur occaecat anim amet sint et dolore excepteur labore ipsum dolor. Irure reprehenderit
         proident tempor elit nostrud consequat adipisicing occaecat sint aliqua Lorem ullamco mollit. Proident non ad
         aliquip magna tempor est duis deserunt adipisicing proident excepteur. Consequat reprehenderit labore mollit
         consequat esse velit. Officia ea laborum adipisicing ullamco occaecat ea aute.
+        {/* cspell:enable */}
       </p>
     </main>
   );
