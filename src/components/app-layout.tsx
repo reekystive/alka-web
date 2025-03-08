06c1dcfaf4c71ea25250aace270ec55b4d451130
ReekyStive <reekystive@gmail.com>
2025-03-08 17:23:49 +0800

    fix: 🎯 adjust main content vertical alignment to start

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -220,7 +220,7 @@ export const AppLayout: FC<AppLayoutProps> = ({ children }) => {
     <div className="flex flex-col md:flex-row h-full w-full overflow-clip relative">
       <DesktopSidebar tabs={tabs} pathname={pathname} onTabItemClick={handleClick} />
       <main className="flex-1 flex flex-col h-full overflow-clip min-w-0">
-        <div className="flex-1 w-full flex flex-col justify-center items-center min-h-0">{children}</div>
+        <div className="flex-1 w-full flex flex-col justify-start items-center min-h-0">{children}</div>
         <MobileNavBarPlaceholder />
       </main>
       <MobileNavBar tabs={tabs} pathname={pathname} onTabItemClick={handleClick} />
